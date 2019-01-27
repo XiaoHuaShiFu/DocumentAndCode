12790fdf7be930e8ddb75477c9796870984e474d
XiaoHuaShiFu <36394669+XiaoHuaShiFu@users.noreply.github.com>
2019-01-27 22:51:51 +0800

    update

--- 数据结构/数据结构复习/312312.c
+++ 数据结构/数据结构复习/312312.c
@@ -1,42 +1,94 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+#define OK 1
+#define ERROR 0
+#define LIST_INIT_SIZE 1000
+#define LISTINCREMENT 10
+#define ElemType int
+#define overflow -2
+typedef struct
+{
+    int *elem;
+    int length;
+    int listsize;
+} SqList;
 
-void maopao (int a[], int n) {
+int Load_Sq(SqList L) //print
+{
     int i;
-    int j;
-    int t;
-    for (i = 1; i < n; i++) {
-        for (j = 0; j < n - i; j++) {
-            if (a[j] > a[j + 1]) {
-                t = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = t;
+    if(L.length==0)
+        printf("The List is empty!");
+    else
+    {
+        printf("The List is: ");
+        for(i=0; i<L.length; i++)
+            printf("%d ",L.elem[i]);
+    }
+    printf("\n");
+    return OK;
+}
+
+int main()
+{
+    SqList T;
+    int a, i,*q,*p,*n;
+    ElemType e, x;//initialization
+    T.elem=(ElemType*)malloc(LIST_INIT_SIZE*sizeof(ElemType));
+    if(!T.elem)
+        exit(overflow);
+    T.length=0;
+    T.listsize=LIST_INIT_SIZE;
+    printf("A Sequence List Has Created.\n");
+     while(1)
+    {
+        printf("1:Insert element\n2:Delete element\n3:Load all elements\n0:Exit\nPlease choose:\n");
+        scanf("%d",&a);
+        switch(a)
+        {
+        case 1:// insert
+            scanf("%d%d",&i,&x);
+            if((i<1)||(i>T.length+1))
+                printf("Insert Error!\n");
+            else
+            {
+                if(T.length>=T.listsize)
+                {
+                    n=(ElemType*)realloc(T.elem,(T.listsize+LISTINCREMENT)*sizeof(ElemType));
+                    if(!n)
+                        exit(overflow);
+                    T.elem=n;
+                    T.listsize+=LISTINCREMENT;
+                }
+                q=&(T.elem[i-1]);
+                if(T.length>0)
+                for(p=&(T.elem[T.length-1]); p>=q; p--)
+                    *(p+1)=*p;
+                *q=x;
+                T.length++;
+                printf("The Element %d is Successfully Inserted!\n", x);
             }
+            break;
+        case 2:// delete
+            scanf("%d",&i);
+            if((i<1)||(i>T.length))
+                printf("Delete Error!\n");
+            else
+            {
+                p=&(T.elem[i-1]);
+                e=*p;
+                q=T.elem+T.length-1;
+                for(p++; p<=q; p++)
+                    *(p-1)=*p;
+                T.length--;
+                printf("The Element %d is Successfully Deleted!\n", e);
+            }
+            break;
+        case 3:
+            Load_Sq(T);
+            break;
+        case 0:
+            return 1;
         }
     }
-}
-
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-//    FILE *fp;
-//    char c;
-//    if ((fp = fopen("case.in", "r")) == NULL) {
-//        return ;
-//    }
-//    while ((c = fgetc(fp)) != EOF)
-
-struct student *reverse(struct student *head) {
-    struct student *p1, *p2, *p3;
-    head = NULL;
-
-}
-
-void main() {
-
 }
